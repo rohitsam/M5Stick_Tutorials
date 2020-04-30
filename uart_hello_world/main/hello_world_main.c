892261ae52b0e81b717e74a5ddd0475a2ebf8775
Rohit samineni <rohit7svp@gmail.com>
2020-04-30 16:33:08 +0530

    first example

---
+++ uart_hello_world/main/hello_world_main.c
@@ -0,0 +1,56 @@
+/* UART Hello_world Example for M5stack
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+
+/**
+ * This is an example which echos any data it receives on UART1 back to the sender,
+ * with hardware flow control turned off. It does not use UART driver event queue.
+ *
+ * - Port: UART1
+ * - Receive (Rx) buffer: on
+ * - Transmit (Tx) buffer: off
+ * - Flow control: off
+ * - Event queue: off
+ * - Pin assignment: see defines below
+ */
+
+
+#define BUF_SIZE (1024)
+#define uart_num UART_NUM_0
+
+    /* Configure parameters of an UART driver,
+     * communication pins and install the driver */
+    uart_config_t uart_config = {
+        .baud_rate = 9600,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,
+    };
+
+
+
+
+
+void app_main(void)
+{
+    uart_driver_install(uart_num, BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(uart_num, &uart_config);
+    // Configure a temporary buffer for the incoming data
+
+    while (1) {
+
+        uart_write_bytes(uart_num, (const char *) "HELLO_WORLD", 11);
+        vTaskDelay(100);
+    }
+
+}
